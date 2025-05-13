29b621fb919f224fb68a482e889ba53abf064f12
Nay Nyein Chan <naychan700@gmail.com>
2025-05-14 01:25:20 +0700

    fix: plugins

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -125,7 +125,7 @@ hooks.Filters.ENV_PATCHES.add_items(
 
 RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
 RUN npm install '@edx/frontend-component-header@git+https://github.com/naynyeinchan/frontend-component-header.git#feat/theme'
-RUN npm install '@edx/brand@npm:@git+https://github.com/naynyeinchan/brand-openedx.git'
+RUN npm install '@edx/brand@git+https://github.com/naynyeinchan/brand-openedx.git'
 
 """,
         )
